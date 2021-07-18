380f1bc17965470ff3ebd0e91add2f47267df278
Thiago Moreira <40751116+AmoreiraT@users.noreply.github.com>
2021-07-18 01:01:56 -0300

    up

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -11,50 +11,104 @@ class About extends React.Component {
         {
           id: "JavaScript_skill",
           content: "JavaScript",
-          porcentage: "90%",
-          value: "90"
+          porcentage: "70%",
+          value: "70"
         },
-        { id: "PHP_skill", content: "PHP", porcentage: "70%", value: "70" },
+        { id: "PHP_skill", content: "Angular", porcentage: "70%", value: "70" },
         {
           id: "ReactJS_skill",
           content: "ReactJS",
-          porcentage: "80%",
-          value: "80"
+          porcentage: "70%",
+          value: "70"
         },
         {
           id: "Python_skill",
-          content: "Python",
+          content: "Flutter",
           porcentage: "75%",
           value: "75"
         },
         {
           id: "VanillaJS_skill",
-          content: "VanillaJS",
+          content: "Adobe PhotoShop",
+          porcentage: "95%",
+          value: "95"
+        },
+         {
+          id: "VanillaJS_skill",
+          content: "Adobe InDesign",
           porcentage: "85%",
           value: "85"
         },
-        {
-          id: "Wordpress_skill",
-          content: "Wordpress",
+          {
+          id: "VanillaJS_skill",
+          content: "Adobe Illustrator",
+          porcentage: "95%",
+          value: "95"
+        },
+           {
+          id: "VanillaJS_skill",
+          content: "Adobe Premiere",
+          porcentage: "75%",
+          value: "75"
+        },
+            {
+          id: "VanillaJS_skill",
+          content: "Adobe After Effects",
           porcentage: "80%",
           value: "80"
-        }
+        },
+             {
+          id: "VanillaJS_skill",
+          content: "Adobe XD",
+          porcentage: "85%",
+          value: "85"
+        },
+              {
+          id: "VanillaJS_skill",
+          content: "Sketch",
+          porcentage: "75%",
+          value: "75"
+        },
+               {
+          id: "VanillaJS_skill",
+          content: "Figma",
+          porcentage: "80%",
+          value: "80"
+        },
+                {
+          id: "VanillaJS_skill",
+          content: "Blender",
+          porcentage: "65%",
+          value: "65"
+        },
+                 {
+          id: "VanillaJS_skill",
+          content: "CINEMA 4D",
+          porcentage: "55%",
+          value: "55"
+        },
+                  {
+          id: "VanillaJS_skill",
+          content: "Unity 3D",
+          porcentage: "75%",
+          value: "75"
+        },
+       
       ],
       about_me: [
         {
           id: "first-p-about",
           content:
-            "Lorem ipsum pariatur consectetur laboris occaecat nulla aliqua irure ad deserunt duis. Eiusmod nulla cupidatat labore sint sit aute dolore irure nostrud ut incididunt. Anim laborum reprehenderit labore magna ut dolore quis irure. Labore ea duis deserunt ullamco irure fugiat deserunt ut nisi ea minim proident. Nisi consectetur do non magna duis aliqua minim minim veniam. In occaecat minim qui consequat elit mollit consectetur non id tempor. Amet adipisicing occaecat tempor culpa quis est duis."
-        },
+"Conquistar meu primeiro computador aos 13 anos, me fez ter que trabalhar para ajudar nos custos da máquina e da internet. Diversos trabalhos, desde farmácia até fazer calçados, me deu a habilidade de adaptação e facilidade de aprendizado em qualquer que fosse a área. Criar artes visuais sempre me motivou, pelo desenho conheci a extensão da minha criatividade. Fui levado pela pintura digital até a modelagem 3D. Conheço o Blender há bastante tempo, também naveguei pelos concorrentes 3DMax, Cinema4D, Maya… Neste momento, com 17 anos, trabalhava para a Secretaria de Cultura da Prefeitura de Pitangui/MG, minha cidade natal. Meu interesse pela arte e cultura se fizeram mais sólidos, então fui estudar Artes Visuais, na Escola de Belas Artes da UFMG."        },
         {
           id: "second-p-about",
           content:
-            "Lorem ipsum pariatur consectetur laboris occaecat nulla aliqua irure ad deserunt duis. Eiusmod nulla cupidatat labore sint sit aute dolore irure nostrud ut incididunt. Anim laborum reprehenderit labore magna ut dolore quis irure. Labore ea duis deserunt ullamco irure fugiat deserunt ut nisi ea minim proident. Nisi consectetur do non magna duis aliqua minim minim veniam. In occaecat minim qui consequat elit mollit consectetur non id tempor. Amet adipisicing occaecat tempor culpa quis est duis."
+            "Mudar do interior para uma capital é um ato de coragem. O resultado deste vestibular só sairia na metade daquele ano, mas no início havia passado para um curso técnico de Jogos Digitais, e fiz um semestre até ingressar na faculdade, mas foi o suficiente para abrir meus olhos para a Programação e o Desenvolvimento de Softwares. Estudei a fundo as artes visuais, trabalhei junto ao espaço f, na Escola de Belas Artes. Foi onde realizei algumas exposições, mas também em outros locais como a Maison De L'Amérique Latine, Paris/França. Entretanto a programação ainda me encantava. Fiz em paralelo matérias do curso de Arte e Tecnologia da mesma faculdade. Desenvolvi obras de arte digital, realidade aumentada, arte eletrônica. Aprendi a fundo linguagens como C#, HTML, CSS e JS, além de softwares como Unity3D para interação digital e Arduino para eletrônicos."
         },
         {
           id: "third-p-about",
           content:
-            "Lorem ipsum pariatur consectetur laboris occaecat nulla aliqua irure ad deserunt duis. Eiusmod nulla cupidatat labore sint sit aute dolore irure nostrud ut incididunt. Anim laborum reprehenderit labore magna ut dolore quis irure. Labore ea duis deserunt ullamco irure fugiat deserunt ut nisi ea minim proident. Nisi consectetur do non magna duis aliqua minim minim veniam. In occaecat minim qui consequat elit mollit consectetur non id tempor. Amet adipisicing occaecat tempor culpa quis est duis."
+            "Amar não é apenas uma relação de prazer, mas também de construção. O que me faz acreditar que amo tudo que faço e trabalho, pois há em mim a necessidade de ser melhor nas áreas que me atravessam. Me tornei Bacharel em 2018 e desde então tenho me dedicado especialmente à programação. Venho trabalhando com ferramentas para Desenvolvimento Web e de Software como Angular, Flutter e ReactNative. Em paralelo também dominando as ferramentas de User Interface, Sketch, AdobeXD e Figma. Encerro falando de amor pois é como percebo o entusiasmo que sinto quando me disponho a aprender ou aprofundar os conhecimentos em tecnologia."
         }
       ]
     };
@@ -112,11 +166,11 @@ class About extends React.Component {
                   <div className="col-md-6">
                     <div className="about-me pt-4 pt-md-0">
                       <div className="title-box-2">
-                        <h5 className="title-left">About Me</h5>
+                        <h5 className="title-left">Minha História</h5>
                       </div>
                       {this.state.about_me.map(content => {
                         return (
-                          <p className="lead" key={content.id}>
+                          <p className="minhaHistoria" key={content.id}>
                             {content.content}
                           </p>
                         );
--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -14,14 +14,17 @@ class Intro extends React.Component {
         <div className="intro-content display-table">
           <div className="table-cell">
             <div className="container">
-              <h1 className="intro-title mb-4">Hello, I am XYZ</h1>
+              <h1 className="intro-title mb-4">Thiago Moreira</h1>
               <p className="intro-subtitle">
                 <span className="text-slider-items"></span>
                 <strong className="text-slider">
                   <Typed
                     strings={[
+
+                      "Digital Artist",
+                      "Video Maker",
+                      "3D Generator",
                       "Front End Developer",
-                      "Back End Developer",
                       "Software Engineer"
                     ]}
                     typeSpeed={80}
@@ -33,11 +36,11 @@ class Intro extends React.Component {
               </p>
               <p className="pt-3">
                 <a
-                  className="btn btn-primary btn js-scroll px-4"
+                  className="btn btn-secondary btn js-scroll px-4"
                   href="#work"
                   role="button"
                 >
-                  View My Work
+                  Veja meus Trabalhos e Projetos
                 </a>
               </p>
             </div>
